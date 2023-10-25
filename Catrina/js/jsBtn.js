4b28e73818de6ca04ae77a281a7bffe5bc99a99b
Evisaspp <104515625+Evisaspp@users.noreply.github.com>
2023-10-25 08:22:02 -0600

    create index.html for my project

---
+++ Catrina/js/jsBtn.js
@@ -0,0 +1,46 @@
+function mostrarcalaca(){
+    document.getElementById("cambiocala").classList.toggle("desaparecer")
+    document.getElementById("cambiocala2").classList.toggle("aparecer")
+    document.getElementById("infocalaca").classList.toggle("aparecer")
+    
+}
+
+function mostrarmural(){
+    document.getElementById("cambioMural").classList.toggle("desaparecer")
+    document.getElementById("cambioMural2").classList.toggle("aparecer")
+    document.getElementById("infoMural").classList.toggle("aparecer")
+    
+}
+
+function mostrarActualidad(){
+    document.getElementById("cambioActua").classList.toggle("desaparecer")
+    document.getElementById("cambioActua2").classList.toggle("aparecer")
+    document.getElementById("infoActua").classList.toggle("aparecer")
+    
+}
+
+
+
+
+
+
+function mostrarcalaca2(){
+    document.getElementById("cambiocala2").classList.toggle("desaparecer")
+    document.getElementById("cambiocala22").classList.toggle("aparecer")
+    document.getElementById("infocalaca2").classList.toggle("aparecer")
+    
+}
+
+function mostrarmural2(){
+    document.getElementById("cambioMural2").classList.toggle("desaparecer")
+    document.getElementById("cambioMural22").classList.toggle("aparecer")
+    document.getElementById("infoMural2").classList.toggle("aparecer")
+    
+}
+
+function mostrarActualidad2(){
+    document.getElementById("cambioActua2").classList.toggle("desaparecer")
+    document.getElementById("cambioActua22").classList.toggle("aparecer")
+    document.getElementById("infoActua2").classList.toggle("aparecer")
+    
+}
